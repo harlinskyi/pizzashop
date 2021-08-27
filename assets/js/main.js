2629ad3153bfb244612752eb10ef1ea9254fa505
Kiril <77387938+harlinskyi@users.noreply.github.com>
2021-08-27 14:40:25 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const baseUrl = `http://harlinskyi.somee.com/api/Pizza`
+let promise = new Promise(async function (resolve, reject) {
+    var pizzas = await fetch(`${baseUrl}/list`)
+        .then(response => response.json());
+    setTimeout(() => resolve(pizzas), 300);
+
+});
